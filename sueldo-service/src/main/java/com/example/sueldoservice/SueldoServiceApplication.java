64de8d6726b88a37c98be80befb7ac84c4cab1a1
Luis Javier Toro Flores <91231015+LuisToroSck@users.noreply.github.com>
2022-11-11 17:21:27 -0300

    Copia de microservicio en Maven

---
+++ sueldo-service/src/main/java/com/example/sueldoservice/SueldoServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.sueldoservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SueldoServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SueldoServiceApplication.class, args);
+	}
+
+}
